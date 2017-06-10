c30a8362b6d1b1a519f949dc2e2b48857d5d6211
gilikarni <gilikarni@campus.technion.ac.il.com>
2017-06-10 19:03:43 +0300

    Changed parser to be a python script

---
+++ src/parser/create_only_sizes_file.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import sys
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("No input file was entered\n")
+    input_file = open(sys.argv[1], 'r')
+    output_file = open("traces_by_size_only", 'w')
+    for line in input_file:
+        split_line = line.split()
+        output_file.write(split_line[-1])
+        output_file.write("\n")
+    input_file.close()
+    output_file.close()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
+
